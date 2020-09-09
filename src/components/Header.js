3e6c1236ed4b4fee7734ac1203d79f9a4112ebcb
ahmed ibrahim <ahmedibrahimhassan654@gmail.com>
2020-09-09 02:28:20 +0200

    edit header

---
+++ src/components/Header.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import './header.css'
+function Header() {
+  return (
+    <div className='header'>
+      <img
+        className='header_logo'
+        src='https://images.app.goo.gl/xmfhD42KmtwDBKtR6'
+        alt='amazone icon'
+      />
+      <div className='header_search'>
+        <input className='header_search_input' />
+
+        {/* icon */}
+      </div>
+      <div className='header_nav'>
+        <div className='header_navoption'>
+          <span className='headeroptionone'>welcomegust</span>
+          <span className='headeroptionone'>signe in</span>
+        </div>
+        <div className='header_navoption'>
+          <span className='headeroptiontwo'>returns</span>
+          <span className='headeroptiontwo'>&order</span>
+        </div>
+        <div className='header_navoption'>
+          <span className='headeroptiontwo'>Your</span>
+          <span className='headeroptiontwo'>Prime</span>
+        </div>
+        <div className='header_navoption'>
+          {/* icon */}
+          {/* number */}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/firbase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyARPvFHRhcWc-tajssYrmabSIwIN1Ph5QY",
+  authDomain: "e-clone-e643f.firebaseapp.com",
+  databaseURL: "https://e-clone-e643f.firebaseio.com",
+  projectId: "e-clone-e643f",
+  storageBucket: "e-clone-e643f.appspot.com",
+  messagingSenderId: "844981236576",
+  appId: "1:844981236576:web:6c5fac1c3a49f9ccad2405",
+  measurementId: "G-JPZ99ERRJC"
+};
