83f396229a854fc5dd4cec1915bcebf34dfb382b
ahmed ibrahim <ahmedibrahimhassan654@gmail.com>
2020-09-13 21:27:49 +0200

    add routing to app component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react'
 
 import './App.css'
 import Header from './components/Header'
-import Home from './components/home/Home';
+import Home from './components/home/Home'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className='App'>
       <Header />
-      <Home/>
+      <Home />
     </div>
   )
 }
