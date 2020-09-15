101185021620205380e2e4875862e1a0e8cbb185
Ahmed Ibrahim <ahmedibrahimhassan654@gmail.com>
2020-09-15 17:00:30 +0200

    fix the stateProvider compomemt worning

--- src/StateProvider.js
+++ src/StateProvider.js
@@ -1,11 +1,10 @@
 import React, { createContext, useContext, useReducer } from "react";
 //prepares the data layer
-export const StateContext = createContext;
+export const StateContext = createContext();
 
 // wrap our app and provide tha data layer
 export const StateProvider = ({ reducer, initialState, children }) => (
-  <StateContext.Provider
-   value={useReducer(reducer, initialState)}>
+  <StateContext.Provider value={useReducer(reducer, initialState)}>
     {children}
   </StateContext.Provider>
 );
